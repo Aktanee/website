625c09972c4f8fd5a63bba37ce4e4286fd6a7698
Antoine Renault <antoine.renault.mmi@gmail.com>
2018-09-17 13:08:26 +0200

    Update: remove initial files

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,20 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
         website
       </h1>
       <h2 class="subtitle">
         Antoine Renault portfolio
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  components: {
-    AppLogo
-  }
+  components: {}
 }
 </script>
 
@@ -56,10 +42,6 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
 
