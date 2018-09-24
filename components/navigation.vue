60aec227209c3d1d47cc27191137d3d4429b2910
Antoine Renault <antoine.renault.mmi@gmail.com>
2018-09-24 15:01:47 +0200

    Update: default navigation SCSS

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="navigation">
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/contact">Contact</router-link>
+      <div class="left">
+        <router-link to="/">Home</router-link>
+      </div>
+      <div class="right">
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </div>
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Navigation'
-}
+  name: "Navigation"
+};
 </script>
+
+<style lang="scss" scoped>
+.navigation {
+  overflow: auto;
+  margin: 0 0 20px 0;
+}
+
+nav {
+  float: right;
+}
+
+.left,
+.right {
+  float: left;
+}
+
+.right {
+  text-align: right;
+}
+
+a {
+  margin: 0 10px 0 0;
+  text-decoration: none;
+  color: black;
+}
+</style>
+
