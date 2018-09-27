88838d64000ae9f6bfc1e5569699c0932408e517
Antoine Renault <antoine.renault.mmi@gmail.com>
2018-09-27 15:04:32 +0200

    Fix: Pre generate test of projects

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,25 +3,29 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'website',
+    title: "website",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Antoine Renault portfolio' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Antoine Renault portfolio"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Raleway'
-      },
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Raleway"
+      }
     ]
   },
-  css: ['normalize.css', '@/assets/scss/base.scss'],
+  css: ["normalize.css", "@/assets/scss/base.scss"],
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -29,16 +33,24 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
+  },
+  generate: {
+    subFolders: false,
+    routes: [
+      "/projects/entourage",
+      "/projects/bootstrap-magic",
+      "/projects/mashup-template",
+      "/projects/le-top-100"
+    ]
   }
-}
-
+};
